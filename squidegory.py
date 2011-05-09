890a9596eeb3029cc770ee973934c93725557f68
jrt369 <james@tanis.us>
2011-05-09 12:00:56 -0400

    Small refactor, created a Squidegory class.

--- squidegory.py
+++ squidegory.py
@@ -4,65 +4,72 @@ from collections import Counter, deque
 from ConfigParser import SafeConfigParser
 import re
 
-def get_category_counter(list, category):
-    request_counter = Counter(list)
-    test_counter = Counter(open(category[1], 'r').readlines())
+class Squidegory:
+    def __init__(self):
+        self.reload()
 
-    for domain in request_counter.keys()[:]:
-        domain_parts = deque(domain.split('.'))
-        domain_construction = domain_parts.pop()
-        while len(domain_parts) > 0:
-            domain_construction = '.'.join((domain_parts.pop(), domain_construction))
-            if not test_counter.has_key(domain_construction + '\n'):
-                del request_counter[domain]
-                break
-    return request_counter
+    def reload(self):
+        self.categories = self.get_categories()
+        self.request_list = self.get_request_list()
 
-def get_unknown_counter(list, categories):
-    request_counter = Counter(list)
-    for key, value in categories:
-        test_counter = Counter(open(value, 'r').readlines())
+    def get_category_counter(self, category):
+        request_counter = Counter(self.request_list)
+        test_counter = Counter(open(category[1], 'r').readlines())
 
         for domain in request_counter.keys()[:]:
             domain_parts = deque(domain.split('.'))
             domain_construction = domain_parts.pop()
             while len(domain_parts) > 0:
                 domain_construction = '.'.join((domain_parts.pop(), domain_construction))
-                if test_counter.has_key(domain_construction + '\n'):
+                if not test_counter.has_key(domain_construction + '\n'):
                     del request_counter[domain]
                     break
-    return request_counter
+        return request_counter
 
-def get_categories():
-    config = SafeConfigParser()
-    config.read(['squidegory.cfg'])
-    return config.items('category')
+    def get_unknown_counter(self):
+        request_counter = Counter(self.request_list)
+        for key, value in self.categories:
+            test_counter = Counter(open(value, 'r').readlines())
 
-def get_request_list():
-    new_request_list = []
-    domain_regex = re.compile('^.*http://(.*?)/')
-    for line in open('/tmp/access.log', 'r'):
-        domain_match = domain_regex.match(line)
-        try:
-            if len(domain_match.group(1).split('.')) > 1:
-                new_request_list.append(domain_match.group(1).strip())
-        except AttributeError:
-            pass
-    return new_request_list
+            for domain in request_counter.keys()[:]:
+                domain_parts = deque(domain.split('.'))
+                domain_construction = domain_parts.pop()
+                while len(domain_parts) > 0:
+                    domain_construction = '.'.join((domain_parts.pop(), domain_construction))
+                    if test_counter.has_key(domain_construction + '\n'):
+                        del request_counter[domain]
+                        break
+        return request_counter
+
+    def get_categories(self):
+        config = SafeConfigParser()
+        config.read(['squidegory.cfg'])
+        return config.items('category')
+
+    def get_request_list(self):
+        new_request_list = []
+        domain_regex = re.compile('^.*http://(.*?)/')
+        for line in open('/tmp/access.log', 'r'):
+            domain_match = domain_regex.match(line)
+            try:
+                if len(domain_match.group(1).split('.')) > 1:
+                    new_request_list.append(domain_match.group(1).strip())
+            except AttributeError:
+                pass
+        return new_request_list
 
 if __name__ == "__main__":
-    categories = get_categories()
-    request_list = get_request_list()
-    print ("total relevant requests: %s" %(len(request_list)))
+    analyze = Squidegory()
+    print("total relevant requests: %s" %(len(analyze.request_list)))
 
-    unknown_counter = get_unknown_counter(request_list, categories)
+    unknown_counter = analyze.get_unknown_counter()
     for domain in unknown_counter.most_common():
-        print (domain)
-    print ("total unique/unknown domains: %s\n\n" %len(unknown_counter))
+        print(domain)
+    print("total unique/unknown domains: %s\n\n" %len(unknown_counter))
 
-    for category in categories:
-        category_counter = get_category_counter(request_list, category)
+    for category in analyze.categories:
+        category_counter = analyze.get_category_counter(category)
         if len(category_counter) > 0:
-            print category[0], len(category_counter), category_counter
+            print(category[0], len(category_counter), category_counter)
         else:
-            print category[0], len(category_counter)
+            print(category[0], len(category_counter))
